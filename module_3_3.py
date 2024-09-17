1731d2adc1c80d66e335eb9504b60791df07c99c
xydZ-Lor <amoevedgar@icloud.com>
2024-09-17 17:05:32 +0300

    Add files via upload

---
+++ module_3_3.py
@@ -0,0 +1,14 @@
+def print_params(a = 1, b = 'строка', c = True):
+    print(a, b, c)
+
+print_params(b = 25)
+print_params(c = [1,2,3])
+
+values_list = [2.6, True, 'привет']
+values_dict = {'a': True, 'b': 'упс', 'c': 25.5}
+
+print_params(*values_list)
+print_params(**values_dict)
+
+values_list_2 = [54.32, 'Строка' ]
+print_params(*values_list_2, 42)
